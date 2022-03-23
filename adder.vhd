0c706d987583e1aa7ac35ddce8a818360f73ff2f
Albert Laursen <alaur20@student.aau.dk>
2022-03-23 09:20:17 +0100

    not working

--- adder.vhd
+++ adder.vhd
@@ -5,7 +5,10 @@ library ieee ;
 entity adder is
   port (
     clock : in std_logic;
+    glob_clock : in std_logic;
+    clk2        : in std_logic;
     add_sub : in std_logic;
+    add_sub_debug : out std_logic;
     dataa : in std_logic_vector (7 downto 0);
     datab : in std_logic_vector (7 downto 0);
     result: out std_logic_vector ( 7 downto 0)
@@ -17,12 +20,24 @@ architecture arch of adder is
     signal u_dataa : unsigned (7 downto 0) := unsigned(dataa); 
     signal u_datab : unsigned (7 downto 0) := unsigned(datab); 
     signal u_result: unsigned (7 downto 0);
+    signal add_sub_sig: std_logic;
 
 begin
-    artih : process( clock )
+    
+    get_add_bit : process( all )
     begin
-        if (falling_edge(clock)) then
-            if add_sub = '1' then
+        if (rising_edge(clk2)) then
+            add_sub_sig <= add_sub;
+        end if ;
+    end process ; -- get_add_bit
+           
+            
+    artih : process( all )
+    begin
+        if (falling_edge(glob_clock) and clock = '1' ) then
+            -- add_sub_sig <= add_sub;
+            add_sub_debug <= add_sub_sig;
+            if add_sub_sig = '1' then
                 u_result <= u_dataa + u_datab;
             else
                 u_result <= u_dataa - u_datab;
