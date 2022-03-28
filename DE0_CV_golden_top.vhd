3caec1b1e5231a65f5bad7a1f4c05e4389da13e9
Martin Boysen <martin-l18@hotmail.com>
2022-03-28 13:07:01 +0200

    Har skrevet ADC kode og comparator/PWM kode sammen

--- DE0_CV_golden_top.vhd
+++ DE0_CV_golden_top.vhd
@@ -22,7 +22,7 @@ entity DE0_CV_golden_top is
 	(
 		CLOCK2_50 : in std_logic;
 		GPIO_0 : in std_logic_vector(35 downto 0);
-		GPIO_1 : out std_logic_vector(35 downto 0);
+		GPIO_1 : out std_logic_vector(35 downto 0); --Clock er sat meget ned for at teste
 		KEY : in std_logic_vector(3 downto 0);
 		LEDR : out std_logic_vector(9 downto 0);
 		SW : in std_logic_vector(9 downto 0)
@@ -35,16 +35,16 @@ architecture ppl_type of DE0_CV_golden_top is
 	signal clk 		: std_logic;
 	signal add_sub_sig : std_logic;
 	signal sel_sig 	: std_logic_vector(2 downto 0);
-	signal clockscale	: unsigned(10 downto 0);
+	signal clockscale	: unsigned(24 downto 0);
 	signal result_sig	: std_logic_vector(7 downto 0);
 	signal GPIO_1OUT	: std_logic_vector(7 downto 0);
 
 	signal saveB : std_logic_vector (7 downto 0);
-	signal duty_cycle : unsigned (3 downto 0);
+	signal duty_cycle : unsigned (3 downto 0) := "0000";
 	signal saveA : std_logic_vector (7 downto 0);
 	signal vej_h : std_logic; 
 	signal comp_out : std_logic_vector (2 downto 0);
-	signal PWM_clk :std_logic_vector (3 downto 0)
+	signal PWM_clk : unsigned (3 downto 0);
 
 	component ADC is
 	port (
@@ -76,37 +76,32 @@ architecture ppl_type of DE0_CV_golden_top is
 begin
 	-- ADC Inst
 	adc_inst : ADC port map (
-			clk 				=> clockscale(10),	-- 1hz clock,
+			clk 				=> clockscale(24),	-- 1hz clock,
 			gpio1(7 downto 0)	=> GPIO_1OUT(7 downto 0),
 			result_sig_out		=> result_sig,
 			add_sub_sig 		=> GPIO_0(0)
 		);
 
 	PWM_comp : PWM_submodule port map (
-			pwm_out => LEDR(0),
+			pwm_out => LEDR(9), --PWM output er sat til en LED
 			duty_cycle => std_logic_vector(duty_cycle),
-			clk => CLOCK_50
+			clk => CLOCK2_50
 		);
 
 	Comp1 : eightBitComparator port map ( 
-			saveA(7 downto 0) => saveA(7 downto 0),
+			saveA(7 downto 0) => result_sig,
 			saveB(7 downto 0) => saveB(7 downto 0),
 			exIn(1) => '1',
 			exOut(2 downto 0) => comp_out(2 downto 0)
 		);
 
 
-	-- Write final adc value to LED
-	display : for i in 0 to 7 generate
-		LEDR(i) <= result_sig(i);
-	end generate ; -- display
-
+	-- Write final adc value to comparator
 
 	-- Write multiplexer output to DAC
-	GPIO_write : for i in 0 to 7 generate
-		GPIO_1(i) <= GPIO_1OUT(i);
-	end generate ; -- GPIO_write 
-
+		GPIO_1(7 downto 0) <= GPIO_1OUT(7 downto 0);
+	-- Sætter ADC resultat ud på LED'er for at teste
+		LEDR(7 downto 0) <= result_sig(7 downto 0);
 
 	-- Scale clock from 50MHz 
 	clockscaler : process( CLOCK2_50 )
@@ -116,11 +111,54 @@ begin
 		end if ;
 	end process ; -- clockscaler
 
-	PWM_clockscaler : process( clockscale(10) )
+	PWM_clockscaler : process( clockscale(24) )
 	begin
-		if rising_edge(clockscale(10)) then
+		if rising_edge(clockscale(24)) then
 			PWM_clk <= PWM_clk + 1 ;
 		end if ;
 	end process ; -- PWM_clockscaler
 	
+
+	MPPT_algoritme : process( all )
+		begin
+			-- MPPT algoritme
+			if rising_edge( PWM_clk(3) ) then
+				--opstart
+				if saveB(7 downto 0) = "00000000" then
+					saveB(7 downto 0) <= result_sig(7 downto 0);
+					duty_cycle <= duty_cycle + 1;
+					vej_h <= '1';
+				end if ;
+
+				--Funktion til højre
+				if vej_h = '1' then
+					if comp_out(0) = '1' or comp_out(1) = '1' then --b>a
+						duty_cycle <= duty_cycle - 1;
+						vej_h <= '0';
+						saveB(7 downto 0) <= result_sig(7 downto 0);
+					end if;
+					if comp_out(2) = '1' then --a>b
+						duty_cycle <= duty_cycle + 1;
+						vej_h <= '1';
+						saveB(7 downto 0) <= result_sig(7 downto 0);
+					end if ;
+					
+				end if ;
+
+				--Funktion til venstre 
+				if vej_h = '0' then
+					if comp_out(0) = '1' then --b>a
+						duty_cycle <= duty_cycle + 1;
+						vej_h <= '1';
+						saveB(7 downto 0) <= result_sig(7 downto 0);
+					end if;
+					if comp_out(2) = '1' or comp_out(1) = '1' then --a>b
+						duty_cycle <= duty_cycle - 1;
+						vej_h <= '0';
+						saveB(7 downto 0) <= result_sig(7 downto 0);
+					end if ;
+				end if;	
+			end if;		
+		end process ; -- MPPT-algoritme
+
 end;
