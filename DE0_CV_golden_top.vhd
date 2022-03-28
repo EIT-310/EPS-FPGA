b14c718b04cbbcf93612c9cbc921e25cf262f035
Martin Boysen <martin-l18@hotmail.com>
2022-03-28 09:13:55 +0200

    I gang med at skrive PWM signal sammen med adc kode

--- DE0_CV_golden_top.vhd
+++ DE0_CV_golden_top.vhd
@@ -39,6 +39,13 @@ architecture ppl_type of DE0_CV_golden_top is
 	signal result_sig	: std_logic_vector(7 downto 0);
 	signal GPIO_1OUT	: std_logic_vector(7 downto 0);
 
+	signal saveB : std_logic_vector (7 downto 0);
+	signal duty_cycle : unsigned (3 downto 0);
+	signal saveA : std_logic_vector (7 downto 0);
+	signal vej_h : std_logic; 
+	signal comp_out : std_logic_vector (2 downto 0);
+	signal PWM_clk :std_logic_vector (3 downto 0)
+
 	component ADC is
 	port (
 		clk         : in std_logic;
@@ -47,15 +54,45 @@ architecture ppl_type of DE0_CV_golden_top is
 		add_sub_sig : in std_logic
 	) ;
 	end component;
+
+	component eightBitComparator is
+		port (
+			saveA: in std_logic_vector (7 downto 0);
+			saveB: in std_logic_vector (7 downto 0);
+			exIn: in std_logic_vector (2 downto 0);
+			exOut: out std_logic_vector (2 downto 0)
+			);
+		end component;
+		
+		component PWM_submodule is
+			port (
+				pwm_out : out std_logic;
+				duty_cycle : in std_logic_vector(3 downto 0);
+				clk : in std_logic
+			);
+		end component;
   
 
 begin
-		-- ADC Inst
-			adc_inst : ADC port map (
+	-- ADC Inst
+	adc_inst : ADC port map (
 			clk 				=> clockscale(10),	-- 1hz clock,
 			gpio1(7 downto 0)	=> GPIO_1OUT(7 downto 0),
 			result_sig_out		=> result_sig,
 			add_sub_sig 		=> GPIO_0(0)
+		);
+
+	PWM_comp : PWM_submodule port map (
+			pwm_out => LEDR(0),
+			duty_cycle => std_logic_vector(duty_cycle),
+			clk => CLOCK_50
+		);
+
+	Comp1 : eightBitComparator port map ( 
+			saveA(7 downto 0) => saveA(7 downto 0),
+			saveB(7 downto 0) => saveB(7 downto 0),
+			exIn(1) => '1',
+			exOut(2 downto 0) => comp_out(2 downto 0)
 		);
 
 
@@ -78,5 +115,12 @@ begin
 			clockscale <= clockscale + 1 ;
 		end if ;
 	end process ; -- clockscaler
+
+	PWM_clockscaler : process( clockscale(10) )
+	begin
+		if rising_edge(clockscale(10)) then
+			PWM_clk <= PWM_clk + 1 ;
+		end if ;
+	end process ; -- PWM_clockscaler
 	
 end;
